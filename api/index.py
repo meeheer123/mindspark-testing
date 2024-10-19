d262388035780b6a5c9cd43f5336d6f38a20c6e6
mihir <mihir.pande22@pccoepune.org>
2024-10-19 09:30:55 +0530

    testing on local

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask
 
 # Define your API endpoint
-MY_API_ENDPOINT = "https://foremost-sweltering-dew.glitch.me/get-errors"
+MY_API_ENDPOINT = "https://16ca-210-212-183-2.ngrok-free.app/get-errors"
 
 # Sentry initialization
 sentry_sdk.init(
